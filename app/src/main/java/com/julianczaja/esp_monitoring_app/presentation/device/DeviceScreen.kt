f47e94cc2baf4b2372a72d2c032ea26e9b6f6e17
julianczaja <julianczaja97@gmail.com>
2023-01-03 15:51:21 +0100

    Implemented ViewPager in DeviceScreen

--- app/src/main/java/com/julianczaja/esp_monitoring_app/presentation/device/DeviceScreen.kt
+++ app/src/main/java/com/julianczaja/esp_monitoring_app/presentation/device/DeviceScreen.kt
@@ -1,111 +1,163 @@
 package com.julianczaja.esp_monitoring_app.presentation.device
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.TabRow
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.google.accompanist.pager.*
 import com.julianczaja.esp_monitoring_app.R
 import com.julianczaja.esp_monitoring_app.components.PhotoCoilImage
 import com.julianczaja.esp_monitoring_app.domain.model.Photo
 import com.julianczaja.esp_monitoring_app.presentation.theme.spacing
 import com.julianczaja.esp_monitoring_app.toPrettyString
+import kotlinx.coroutines.launch
+
 
 const val DEFAULT_PHOTO_HEIGHT = 150
 
-@OptIn(ExperimentalLifecycleComposeApi::class)
+enum class DevicePage(
+    val index: Int,
+    @StringRes val titleId: Int,
+    @DrawableRes val drawableId: Int,
+) {
+    Photos(0, R.string.photos_tab_label, R.drawable.ic_baseline_photo_24),
+    Saved(1, R.string.saved_tab_label, R.drawable.ic_baseline_bookmarks_24),
+    Settings(2, R.string.settings_tab_label, R.drawable.ic_baseline_settings_24)
+}
+
+@OptIn(ExperimentalLifecycleComposeApi::class, ExperimentalPagerApi::class)
 @Composable
 fun DeviceScreen(
     viewModel: DeviceScreenViewModel = hiltViewModel(),
 ) {
     val uiState by viewModel.deviceUiState.collectAsStateWithLifecycle()
+    val coroutineScope = rememberCoroutineScope()
+    val pagerState = rememberPagerState()
+    val pages = listOf(DevicePage.Photos, DevicePage.Saved, DevicePage.Settings)
 
-    DeviceScreenContent(
-        uiState = uiState,
-        updatePhotos = viewModel::updatePhotos
-    )
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        TabRow(
+            selectedTabIndex = pagerState.currentPage,
+            backgroundColor = MaterialTheme.colorScheme.background,
+            divider = { Divider(color = MaterialTheme.colorScheme.surfaceVariant) },
+            indicator = { tabPositions ->
+                TabRowDefaults.Indicator(
+                    modifier = Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
+                )
+            },
+            tabs = {
+                pages.forEach { devicePage ->
+                    Tab(
+                        icon = {
+                            Icon(
+                                painter = painterResource(id = devicePage.drawableId),
+                                contentDescription = null
+                            )
+                        },
+                        text = { Text(stringResource(id = devicePage.titleId)) },
+                        selected = pagerState.currentPage == devicePage.index,
+                        selectedContentColor = MaterialTheme.colorScheme.primary,
+                        unselectedContentColor = MaterialTheme.colorScheme.onSurface,
+                        onClick = {
+                            coroutineScope.launch {
+                                pagerState.animateScrollToPage(devicePage.index)
+                            }
+                        },
+                    )
+                }
+            },
+            modifier = Modifier.zIndex(1f) // to hide PullRefreshIndicator under TabRow
+        )
+        HorizontalPager(
+            count = pages.size,
+            state = pagerState,
+            modifier = Modifier.fillMaxSize()
+        ) { page ->
+            when (page) {
+                DevicePage.Photos.index -> DevicePhotosScreenContent(uiState, viewModel::updatePhotos)
+                DevicePage.Saved.index -> DeviceSavedScreenContent()
+                DevicePage.Settings.index -> DeviceSettingsScreenContent()
+            }
+        }
+    }
 }
 
+@OptIn(ExperimentalMaterialApi::class, ExperimentalPagerApi::class)
 @Composable
-fun DeviceScreenContent(
+fun PagerScope.DevicePhotosScreenContent(
     uiState: DeviceScreenUiState,
     updatePhotos: () -> Unit,
 ) {
-    Column(
-        verticalArrangement = Arrangement.SpaceBetween,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()
+    val pullRefreshState = rememberPullRefreshState(uiState.isRefreshing, updatePhotos)
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .pullRefresh(pullRefreshState)
     ) {
-        when (uiState) {
-            DeviceScreenUiState.Loading -> {
-                Box(modifier = Modifier.fillMaxSize()) {
-                    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
-                }
-            }
-            is DeviceScreenUiState.Success -> if (uiState.photos.isNotEmpty()) {
-                PhotosGrid(uiState.photos)
+        when (uiState.devicePhotosUiState) {
+            DevicePhotosUiState.Loading -> CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+            is DevicePhotosUiState.Success -> if (uiState.devicePhotosUiState.photos.isNotEmpty()) {
+                PhotosLazyGrid(photos = uiState.devicePhotosUiState.photos)
             } else {
                 Text(
-                    text = "There are no photos taken by this device :(",
+                    text = stringResource(R.string.no_photos),
                     style = MaterialTheme.typography.headlineSmall,
                 )
             }
-            is DeviceScreenUiState.Error -> {
-                Box(Modifier.fillMaxSize()) {
-                    Text(
-                        text = stringResource(uiState.messageId),
-                        color = MaterialTheme.colorScheme.error,
-                        style = MaterialTheme.typography.headlineSmall,
-                        modifier = Modifier.align(Alignment.Center)
-                    )
-                }
-            }
+            is DevicePhotosUiState.Error -> Text(
+                text = stringResource(uiState.devicePhotosUiState.messageId),
+                color = MaterialTheme.colorScheme.error,
+                style = MaterialTheme.typography.headlineSmall,
+                modifier = Modifier.align(Alignment.Center)
+            )
         }
-        UpdateButton(updatePhotos)
+        PullRefreshIndicator(uiState.isRefreshing, pullRefreshState, Modifier.align(Alignment.TopCenter))
     }
 }
 
 @Composable
-private fun UpdateButton(
-    onClick: () -> Unit,
-) {
-    Button(
-        onClick = onClick,
-        modifier = Modifier
-            .height(IntrinsicSize.Min)
-            .width(200.dp)
-            .padding(MaterialTheme.spacing.medium)
-    ) {
-        Text(text = stringResource(R.string.update_photos_label))
-    }
-}
-
-@Composable
-private fun ColumnScope.PhotosGrid(
+private fun BoxScope.PhotosLazyGrid(
+    modifier: Modifier = Modifier,
     photos: List<Photo>,
+    minSize: Dp = DEFAULT_PHOTO_HEIGHT.dp,
 ) {
     LazyVerticalGrid(
-        columns = GridCells.Adaptive(DEFAULT_PHOTO_HEIGHT.dp),
+        columns = GridCells.Adaptive(minSize),
         contentPadding = PaddingValues(MaterialTheme.spacing.medium),
         horizontalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.medium, Alignment.CenterHorizontally),
         verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.medium, Alignment.Top),
-        modifier = Modifier.weight(1f)
+        modifier = modifier
     ) {
         items(items = photos, key = { it.dateTime }) {
             Box {
                 PhotoCoilImage(
                     url = it.url,
-                    height = DEFAULT_PHOTO_HEIGHT.dp,
+                    height = minSize,
                     onClick = {
                         // TODO
                     }
@@ -128,3 +180,19 @@ private fun ColumnScope.PhotosGrid(
     }
 }
 
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun PagerScope.DeviceSavedScreenContent() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Text(text = "Saved", modifier = Modifier.align(Alignment.Center))
+    }
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun PagerScope.DeviceSettingsScreenContent() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Text(text = "Settings", modifier = Modifier.align(Alignment.Center))
+    }
+}
--- app/src/main/java/com/julianczaja/esp_monitoring_app/presentation/device/DeviceScreenViewModel.kt
+++ app/src/main/java/com/julianczaja/esp_monitoring_app/presentation/device/DeviceScreenViewModel.kt
@@ -24,35 +24,53 @@ class DeviceScreenViewModel @Inject constructor(
 
     private val apiError = MutableStateFlow<Int?>(null)
 
+    private val isRefreshing = MutableStateFlow(false)
+
     val deviceUiState: StateFlow<DeviceScreenUiState> = deviceUiState()
         .stateIn(
             scope = viewModelScope,
             started = SharingStarted.WhileSubscribed(5_000),
-            initialValue = DeviceScreenUiState.Loading
+            initialValue = DeviceScreenUiState(DevicePhotosUiState.Loading, true)
         )
 
-    private fun deviceUiState(): Flow<DeviceScreenUiState> = combine(photosStream(), apiError) { photos, apiErr ->
+    init {
+        updatePhotos()
+    }
+
+    private fun deviceUiState(): Flow<DeviceScreenUiState> = combine(photosStream(), apiError, isRefreshing) { photos, apiErr, refreshing ->
         return@combine if (apiErr != null) {
-            DeviceScreenUiState.Error(apiErr)
+            DeviceScreenUiState(DevicePhotosUiState.Error(apiErr), refreshing)
         } else {
-            photos
+            DeviceScreenUiState(photos, refreshing)
         }
     }
 
     private fun photosStream() = photoRepository.getAllPhotosLocal(deviceArgs.deviceId)
-        .onStart { DeviceScreenUiState.Loading }
-        .catch { DeviceScreenUiState.Error(it.getErrorMessageId()) }
-        .map { DeviceScreenUiState.Success(photos = it) }
+        .onStart { DevicePhotosUiState.Loading }
+        .catch { DevicePhotosUiState.Error(it.getErrorMessageId()) }
+        .map { DevicePhotosUiState.Success(photos = it) }
 
     fun updatePhotos() = viewModelScope.launch(Dispatchers.IO) {
+        isRefreshing.update { true }
         photoRepository.updateAllPhotosRemote(deviceArgs.deviceId) // TODO: Check if it's not in progress already
-            .onFailure { apiError.emit(it.getErrorMessageId()) }
-            .onSuccess { apiError.emit(null) }
+            .onFailure {
+                apiError.emit(it.getErrorMessageId())
+                isRefreshing.update { false }
+            }
+            .onSuccess {
+                apiError.emit(null)
+                isRefreshing.update { false }
+            }
     }
 }
 
-sealed interface DeviceScreenUiState {
-    data class Success(val photos: List<Photo>) : DeviceScreenUiState
-    object Loading : DeviceScreenUiState
-    data class Error(@StringRes val messageId: Int) : DeviceScreenUiState
+data class DeviceScreenUiState(
+    val devicePhotosUiState: DevicePhotosUiState,
+    val isRefreshing: Boolean,
+)
+
+sealed interface DevicePhotosUiState {
+    data class Success(val photos: List<Photo>) : DevicePhotosUiState
+    object Loading : DevicePhotosUiState
+    data class Error(@StringRes val messageId: Int) : DevicePhotosUiState
 }
