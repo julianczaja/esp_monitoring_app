b1e4adb4b96e01923ff259e4cbbb570203960b06
julianczaja <julianczaja97@gmail.com>
2024-06-13 10:49:48 +0200

    Implemented scroll to top button
    When user is on DeviceScreen and scrolls down
    more than 20 photos, a button appears with
    arrow up and circular progress bar filled
    based on number of scrolled items.

--- app/src/main/java/com/julianczaja/esp_monitoring_app/components/SelectablePhotosLazyGrid.kt
+++ app/src/main/java/com/julianczaja/esp_monitoring_app/components/SelectablePhotosLazyGrid.kt
@@ -1,5 +1,8 @@
 package com.julianczaja.esp_monitoring_app.components
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -7,6 +10,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -28,9 +32,18 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.res.painterResource
@@ -45,9 +58,11 @@ import com.julianczaja.esp_monitoring_app.domain.model.SelectablePhoto
 import com.julianczaja.esp_monitoring_app.presentation.devicephotos.DEFAULT_PHOTO_WIDTH
 import com.julianczaja.esp_monitoring_app.presentation.theme.shape
 import com.julianczaja.esp_monitoring_app.presentation.theme.spacing
+import kotlinx.coroutines.launch
 import java.time.LocalDate
 import java.time.LocalDateTime
 
+private const val NUMBER_OF_ITEMS_TO_SCROLL_TO_SHOW_SCROLL_TO_TOP_BUTTON = 20
 
 @Composable
 fun SelectablePhotosLazyGrid(
@@ -60,72 +75,135 @@ fun SelectablePhotosLazyGrid(
     onPhotoLongClick: (SelectablePhoto) -> Unit,
     onSelectDeselectAllClick: (LocalDate) -> Unit,
 ) {
-    LazyVerticalGrid(
-        modifier = modifier,
-        columns = GridCells.Adaptive(minSize),
-        state = state,
-        contentPadding = PaddingValues(MaterialTheme.spacing.medium),
-        horizontalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.medium),
-        verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.medium)
-    ) {
-        dateGroupedSelectablePhotos.onEachIndexed { index, (localDate, photos) ->
-            header(key = localDate) {
-                Column {
-                    if (index > 0) {
-                        HorizontalDivider(
-                            modifier = Modifier.padding(vertical = MaterialTheme.spacing.medium)
-                        )
-                    }
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.spacedBy(
-                                space = MaterialTheme.spacing.small,
-                                alignment = Alignment.Start
-                            )
-                        ) {
-                            Icon(
-                                painter = painterResource(id = R.drawable.ic_calendar),
-                                contentDescription = null
-                            )
-                            Text(
-                                text = localDate.toString(),
-                                style = MaterialTheme.typography.bodyMedium
-                            )
-                        }
-                        val allPhotosWithDateAreSelected = photos
-                            .filter { it.photo.dateTime.toLocalDate() == localDate }
-                            .all { it.isSelected }
+    val coroutineScope = rememberCoroutineScope()
+    val lastVisibleItemIndex by remember {
+        derivedStateOf {
+            state.layoutInfo.visibleItemsInfo.lastOrNull()?.index ?: 0
+        }
+    }
+    val isScrollUpVisible by remember {
+        derivedStateOf {
+            lastVisibleItemIndex > NUMBER_OF_ITEMS_TO_SCROLL_TO_SHOW_SCROLL_TO_TOP_BUTTON
+        }
+    }
+    val scrollProgress by remember(dateGroupedSelectablePhotos) {
+        derivedStateOf {
+            lastVisibleItemIndex / dateGroupedSelectablePhotos.values.flatten().size.toFloat()
+        }
+    }
 
-                        val selectDeselectIcon = when (allPhotosWithDateAreSelected) {
-                            true -> R.drawable.ic_deselect_all
-                            false -> R.drawable.ic_select_all
-                        }
-                        IconButton(
-                            modifier = Modifier.size(24.dp),
-                            onClick = { onSelectDeselectAllClick(localDate) }
-                        ) {
-                            Icon(
-                                painter = painterResource(id = selectDeselectIcon),
-                                contentDescription = null
+    Box(modifier) {
+        LazyVerticalGrid(
+            modifier = modifier,
+            columns = GridCells.Adaptive(minSize),
+            state = state,
+            contentPadding = PaddingValues(MaterialTheme.spacing.medium),
+            horizontalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.medium),
+            verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.medium)
+        ) {
+            dateGroupedSelectablePhotos.onEachIndexed { index, (localDate, photos) ->
+                header(key = localDate) {
+                    Column {
+                        if (index > 0) {
+                            HorizontalDivider(
+                                modifier = Modifier.padding(vertical = MaterialTheme.spacing.medium)
                             )
+                        }
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.spacedBy(
+                                    space = MaterialTheme.spacing.small,
+                                    alignment = Alignment.Start
+                                )
+                            ) {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.ic_calendar),
+                                    contentDescription = null
+                                )
+                                Text(
+                                    text = localDate.toString(),
+                                    style = MaterialTheme.typography.bodyMedium
+                                )
+                            }
+                            val allPhotosWithDateAreSelected = photos
+                                .filter { it.photo.dateTime.toLocalDate() == localDate }
+                                .all { it.isSelected }
+
+                            val selectDeselectIcon = when (allPhotosWithDateAreSelected) {
+                                true -> R.drawable.ic_deselect_all
+                                false -> R.drawable.ic_select_all
+                            }
+                            IconButton(
+                                modifier = Modifier.size(24.dp),
+                                onClick = { onSelectDeselectAllClick(localDate) }
+                            ) {
+                                Icon(
+                                    painter = painterResource(id = selectDeselectIcon),
+                                    contentDescription = null
+                                )
+                            }
                         }
                     }
                 }
+                items(photos, key = { it.photo.dateTime }) { selectablePhoto ->
+                    SelectableDevicePhoto(
+                        selectablePhoto = selectablePhoto,
+                        isSelectionMode = isSelectionMode,
+                        minSize = minSize,
+                        onClick = onPhotoClick,
+                        onLongClick = onPhotoLongClick
+                    )
+                }
             }
-            items(photos, key = { it.photo.dateTime }) { selectablePhoto ->
-                SelectableDevicePhoto(
-                    selectablePhoto = selectablePhoto,
-                    isSelectionMode = isSelectionMode,
-                    minSize = minSize,
-                    onClick = onPhotoClick,
-                    onLongClick = onPhotoLongClick
-                )
-            }
+        }
+        ScrollToTopButton(
+            isVisible = isScrollUpVisible,
+            scrollProgress = scrollProgress,
+            onClicked = { coroutineScope.launch { state.animateScrollToItem(0) } }
+        )
+    }
+}
+
+@Composable
+private fun BoxScope.ScrollToTopButton(
+    isVisible: Boolean,
+    scrollProgress: Float,
+    onClicked: () -> Unit
+) {
+    AnimatedVisibility(
+        modifier = Modifier
+            .align(Alignment.BottomCenter)
+            .padding(MaterialTheme.spacing.large),
+        visible = isVisible,
+        enter = slideInVertically(initialOffsetY = { it }),
+        exit = slideOutVertically(targetOffsetY = { it * 2 })
+    ) {
+        val progressColor = MaterialTheme.colorScheme.onPrimaryContainer
+
+        IconButton(
+            onClick = onClicked,
+            modifier = Modifier
+                .drawWithContent {
+                    val strokeWidth = 3.dp.toPx()
+                    drawContent()
+                    drawArc(
+                        color = progressColor,
+                        size = Size(width = size.width - strokeWidth, height = size.height - strokeWidth),
+                        topLeft = Offset(strokeWidth / 2f, strokeWidth / 2f),
+                        startAngle = 270f,
+                        sweepAngle = scrollProgress * 360f,
+                        useCenter = false,
+                        style = Stroke(width = strokeWidth, cap = StrokeCap.Round)
+                    )
+                }
+                .background(MaterialTheme.colorScheme.primaryContainer, CircleShape)
+        ) {
+            Icon(painter = painterResource(id = R.drawable.ic_arrow_up), contentDescription = null)
         }
     }
 }
